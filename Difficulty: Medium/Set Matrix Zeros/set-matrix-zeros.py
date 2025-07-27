2a50af65a087e1deda444d49eb4d92df47d312ba
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2025-07-27 18:58:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.py
@@ -0,0 +1,16 @@
+class Solution:
+    def setMatrixZeroes(self, mat):
+        n, m = len(mat), len(mat[0])
+        zero_rows = set()
+        zero_cols = set()
+
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
+
+        for i in range(n):
+            for j in range(m):
+                if i in zero_rows or j in zero_cols:
+                    mat[i][j] = 0
