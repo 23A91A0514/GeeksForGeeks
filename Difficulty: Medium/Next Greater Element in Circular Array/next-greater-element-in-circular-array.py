d25e2ab1ea61cf5b067fceab87cdc425ddd62253
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2025-09-18 19:36:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.py
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.py
@@ -1,16 +1,13 @@
 class Solution:
-    def nextLargerElement(self, arr):
-        n = len(arr)
-        res = [-1] * n
+    def nextGreater(self, arr):
+        # code here
+        narr = arr + arr
+        res = [-1] * len(arr)
         stack = []
-        
-        for i in range(2 * n - 1, -1, -1):
-            current = arr[i % n]
-            while stack and stack[-1] <= current:
-                stack.pop()
-            if i < n:
-                if stack:
-                    res[i] = stack[-1]
-            stack.append(current)
-        
+        for i in range(len(narr)):
+            while stack and narr[stack[-1]] < narr[i]:
+                ind = stack.pop()
+                if ind < len(res):
+                    res[ind] = narr[i]
+            stack.append(i)
         return res
