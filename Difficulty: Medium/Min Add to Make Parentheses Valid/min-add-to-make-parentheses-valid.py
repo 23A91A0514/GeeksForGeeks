16627d661ea20ba55f3a1425b68d95f10382cccb
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2025-09-19 19:21:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minParentheses(self, s):
+        st = []
+        count = 0
+        for char in s:
+            if char == '(':  #add to stack 
+                st.append(char)
+            elif char == ')':
+                if not st: #check if the stack is empty
+                    count +=1
+                elif st: #if stack not empty,then pop the top element as it has matching paranthesis
+                    st.pop()
+        return len(st)+count
