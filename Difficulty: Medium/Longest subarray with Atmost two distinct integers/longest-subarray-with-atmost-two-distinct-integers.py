c4a0b4a46bda3b8b146d9b4c078ac2d6cc1b7e08
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2026-03-03 20:38:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
@@ -1,20 +1,27 @@
 class Solution:
     def totalElements(self, arr):
         from collections import defaultdict
-        
-        left = 0
-        count = defaultdict(int)
-        max_len = 0
 
-        for right in range(len(arr)):
-            count[arr[right]] += 1
-            
-            while len(count) > 2:
-                count[arr[left]] -= 1
-                if count[arr[left]] == 0:
-                    del count[arr[left]]
+        n = len(arr)
+        if n == 0:
+            return 0
+
+        left = 0
+        max_len = 0
+        freq = defaultdict(int)  
+
+        for right in range(n):
+            freq[arr[right]] += 1
+
+        
+            while len(freq) > 2:
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
                 left += 1
 
+         
             max_len = max(max_len, right - left + 1)
-        
+
         return max_len
+
