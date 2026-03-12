d45b21e3d012a27374c44b214b6dfe18a488e96f
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2026-03-12 21:35:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxLength(self, arr):
+        mx=0
+        sm=0
+        seen={0:-1}
+        for ix,ve in enumerate(arr):
+            sm+=ve
+            if sm in seen:
+                mx=max(mx,ix-seen[sm])
+            if sm not in seen:
+                seen[sm]=ix
+        return mx
