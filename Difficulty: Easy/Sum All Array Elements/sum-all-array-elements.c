73a3e8decb797e08ff6104cede4fd4052f2e9f8c
Purna Mahesh Dhulipudi <23A91A0514@aec.edu.in>
2026-05-09 18:58:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.c
@@ -0,0 +1,9 @@
+// User function template for C
+ int arraySum(int arr[], int size) {
+        // Code here
+        int sum=0;
+        for(int i=0;i<size;i++){
+            sum +=arr[i];
+        }
+        return sum;
+    }
